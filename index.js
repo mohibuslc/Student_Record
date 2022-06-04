4bbd1903f31d8a2edd4cc9ac1ee1e3b5e29b0cbd
mohibuslc <mohib_uslc@outlook.com>
2022-06-04 23:06:49 +0100

    Student_Record

--- index.js
+++ index.js
@@ -10,13 +10,55 @@ var Sgroup = document.getElementById("group");
 
 var btn = document.getElementById("btn_record");
 
+
+
 btn.addEventListener('click', function(para){
 
     para.preventDefault();
 
-    console.log(Sname.value)
+    console.log(Sname.value)  
+    
+    if (Sname.value === ''){
+
+        alert(" Please emter Name")
+
+    }    else if ( Sroll.value === ''){
+
+        alert(" Please Enter roll")
+    }else
+    
+    // Auto make Table with JavaScript : 
+    
+    {
+        var tbody = document.getElementById('tbody');
+        var tr = document.createElement('tr');
+
+        // for student name 
+        var td = document.createElement('td');
+
+        tr.appendChild(td);
+
+        td.innerHTML = Sname.value ;
+
+        // for Student ID 
+       
+        var td1 = document.createElement('td'); 
+        tr.appendChild(td1);
+        td1.innerHTML = Sroll.value ; 
+        
+        // for Student Group
+
+        var td3 = document.createElement('td'); 
+        tr.appendChild(td3);
+        td3.innerHTML = Sgroup.value ; 
+
+        tbody.appendChild(tr)
+
+
 
 
 
     
+    
+    }
 })
